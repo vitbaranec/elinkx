bc5c8a17fea96708e06e56e86b835b6d5b12fb95
vitbaranec <60390107+vitbaranec@users.noreply.github.com>
2021-02-27 15:18:20 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#skript je nutné spustit přes příkazový řádek - do cmd zadat chmod +x start.sh a poté ./start.sh
+#skript spustí docker-compose up
+#do tabulky jdou přidat nebo upravovat položky přes funkce api, pro zkoušku funkcí api používám tool Postman
+
+
+docker-compose up -d
+echo "Tabulka poslouchá na portu 3000 a api na portu 8080"
+
+
