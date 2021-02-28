7091b91c8bed5fe81cba677ec36b97adf8b9ea4b
vitbaranec <60390107+vitbaranec@users.noreply.github.com>
2021-02-28 21:14:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
+docker-compose up -d
+echo "Tabulka poslouchá na portu 3000 a api na portu 8080"
+
 #skript je nutné spustit přes příkazový řádek - do cmd zadat chmod +x start.sh a poté ./start.sh
 #skript spustí docker-compose up
 #do tabulky jdou přidat nebo upravovat položky přes funkce api, pro zkoušku funkcí api používám tool Postman
-
-
-docker-compose up -d
-echo "Tabulka poslouchá na portu 3000 a api je možné testovat na portu 8080"
 
 
